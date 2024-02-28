affd9127d3fef51f16664424ae1d49b21c97d125
Salemelmabrouk <149633706+Salemelmabrouk@users.noreply.github.com>
2024-02-28 21:26:19 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+   document.addEventListener("DOMContentLoaded", function() {
+            
+            var paragraph = document.createElement("p");
+            
+            paragraph.textContent = "DOM load success";
+            
+            document.body.appendChild(paragraph);
+        });
